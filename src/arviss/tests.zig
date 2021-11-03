f752f37228441f37c188978a6ad502f5910b4a82
Rod Hyde <rod.hyde@gmail.com>
2021-11-03 22:46:37 +0000

    Add test for "mulh".

--- src/arviss/tests.zig
+++ src/arviss/tests.zig
@@ -1171,3 +1171,32 @@ test "sll (shift left logical)" {
     _ = ArvissExecute(&cpu, (0 << 25) | encodeRs2(rs2) | encodeRs1(rs1) | (0b001 << 12) | encodeRd(0) | @enumToInt(arviss.ArvissOpcode.opOP));
     try testing.expectEqual(@intCast(u32, 0), cpu.xreg[0]);
 }
+
+test "mulh" { // 'M' extension.
+    // MULH performs a 32-bit x 32-bit (signed x signed) multiplication of rs1 by rs2 and places the upper 32 bits of the 64 bit
+    // product in the destination register.
+    var cpu = Cpu();
+
+    // rd <- upper32(rs1 * rs2), pc += 4
+    var pc: u32 = cpu.pc;
+    const rd: u32 = 5;
+    const rs1: u32 = 13;
+    const rs2: u32 = 14;
+    cpu.xreg[rs1] = 16777216; // 2 ** 24
+    cpu.xreg[rs2] = 0xf5ffec40; // -(2 ** 24)
+    
+    const product: i64 = @intCast(i64, @bitCast(i32, cpu.xreg[rs1])) * @intCast(i64, @bitCast(i32, cpu.xreg[rs2]));
+    const expected: i32 = @intCast(i32, product >> 32);
+
+    _ = ArvissExecute(&cpu, (0b0000001 << 25) | encodeRs2(rs2) | encodeRs1(rs1) | (0b001 << 12) | encodeRd(rd) | @enumToInt(arviss.ArvissOpcode.opOP));
+
+    // rd <- upper32(rs1 * rs2)
+    try testing.expectEqual(@bitCast(u32, expected), cpu.xreg[rd]);
+
+    // pc <- pc + 4
+    try testing.expectEqual(pc + 4, cpu.pc);
+
+    // x0 <- 0
+    _ = ArvissExecute(&cpu, (0b0000001 << 25) | encodeRs2(rs2) | encodeRs1(rs1) | (0b001 << 12) | encodeRd(0) | @enumToInt(arviss.ArvissOpcode.opOP));
+    try testing.expectEqual(@intCast(u32, 0), cpu.xreg[0]);
+}
